fbe592cb9905d57f36bd526216bef05f6beed473
Windows10 <prem.shyam01@gmail.com>
2020-05-28 20:57:20 +0530

    changed Corporate Event Schema

--- models/CorporateEvent.js
+++ models/CorporateEvent.js
@@ -6,6 +6,7 @@ mongoose.set("useFindAndModify", false);
 const corporateEventSchema = new Schema({
   filterName: { type: String, required: true, unique: true, dropDups: true },
   image: { type: String, required: true },
+  description: { type: String, required: false },
 });
 
 module.exports = mongoose.model("CorporateEvent", corporateEventSchema);
