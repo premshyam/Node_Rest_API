724e4377317b439dd2394bfae784213b0d4b30cf
Windows10 <prem.shyam01@gmail.com>
2020-08-09 17:35:14 +0530

    Updates cart controller and cart route as per cart schema

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -3,103 +3,9 @@ const Cart = require("../models/Cart");
 //module to catch request validation Result
 const { validationResult } = require("express-validator");
 
-exports.add_item = (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
-      message: "Server side validation failed",
-      errors: errors.array(),
-    });
-  }
-  const item = {
-    menu_id: req.body.menu_id,
-    cartItems: req.body.cartItems,
-    quantity: req.body.quantity,
-  };
-  Cart.findOne({ customer_id: req.body.userId })
-    .then((cartObj) => {
-      if (cartObj) {
-        // console.log(cartObj.cartItems);
-        // check if cartItems is empty
-        if (cartObj.cart.length) {
-          if (cartObj.cart.some((element) => element.menu_id == item.menu_id)) {
-            let index = cartObj.cart.findIndex(
-              (i) => i.menu_id == item.menu_id
-            );
-            cartObj.cart[index].quantity = item.quantity;
-            cartObj.cart[index].cartItems = item.cartItems;
-          } else {
-            cartObj.cart.push(item);
-          }
-        } else {
-          cartObj.cart.push(item);
-        }
-        return cartObj.save();
-      } else {
-        res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
-          message: "Cart Not Found",
-          errors: [],
-        });
-      }
-    })
-    .then((cart) => {
-      res.json({
-        message: "Added items to Cart",
-        cart: cart,
-      });
-    })
-    .catch((err) => {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: "Something went wrong",
-        errors: err,
-      });
-    });
-};
-
-exports.remove_item = (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
-      message: "Server side validation failed",
-      errors: errors.array(),
-    });
-  }
-  const item = {
-    menu_id: req.body.menu_id,
-  };
-  Cart.findOne({ customer_id: req.body.userId })
-    .then((cartObj) => {
-      if (cartObj) {
-        // console.log(cartObj.cartItems);
-        if (cartObj.cart.some((element) => element.menu_id == item.menu_id)) {
-          let index = cartObj.cart.findIndex((i) => i.menu_id == item.menu_id);
-          cartObj.cart.splice(index, 1);
-        }
-        return cartObj.save();
-      } else {
-        res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
-          message: "Cart Not Found",
-          errors: [],
-        });
-      }
-    })
-    .then((cart) => {
-      res.json({
-        message: "Menu removed from Cart",
-        cart: cart,
-      });
-    })
-    .catch((err) => {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: "Something went wrong",
-        errors: err,
-      });
-    });
-};
-
 // Customer Cart Items
-exports.cart_items = async (req, res) => {
-  await Cart.findOne({ customer_id: req.body.userId })
+exports.getCart = async (req, res) => {
+  await Cart.findOne({ customer: req.body.userId })
     .then((result) => {
       if (result) {
         res.json({
@@ -122,11 +28,12 @@ exports.cart_items = async (req, res) => {
 };
 
 // Delete Cart Item
-exports.empty_cart = async (req, res) => {
-  await Cart.findOne({ customer_id: req.body.userId })
+exports.updateCart = async (req, res) => {
+  await Cart.findOne({ customer: req.body.userId })
     .then((cartObj) => {
       if (cartObj) {
-        cartObj.cart = [];
+        cartObj.caterer = req.body.caterer;
+        cartObj.cartItems = req.body.cartItems;
         return cartObj.save();
       } else {
         res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
@@ -137,7 +44,7 @@ exports.empty_cart = async (req, res) => {
     })
     .then((cartObj) => {
       res.json({
-        message: "Cart empty",
+        message: "Cart updated",
         cart: cartObj,
       });
     })
--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,57 +1,11 @@
 module.exports = (app) => {
-  const cart_controller = require("../controllers/cart.controller");
+  const cartController = require("../controllers/cart.controller");
   const Menu = require("../models/Menu");
   const isAuth = require("../middleware/is-auth");
   const { body, check } = require("express-validator");
 
-  // Add Item to Cart
-  app.post(
-    "/api/add_item",
-    isAuth,
-    [
-      body("menu_id", "not a valid menu").custom((menuId) => {
-        return Menu.findById(menuId).then((result) => {
-          if (result && result !== null) {
-            //
-            // console.log(result);
-            return true;
-          } else {
-            //
-            return Promise.reject("not a valid menu");
-          }
-        });
-      }),
-      body("quantity", "quantity not valid").isNumeric(),
-    ],
-    cart_controller.add_item
-  );
-
-  // Remove Item from Cart
-  app.post(
-    "/api/remove_item",
-    isAuth,
-    [
-      body("menu_id", "not a valid menu").custom((menuId) => {
-        return Menu.findById(menuId).then((result) => {
-          if (result && result !== null) {
-            //
-            console.log(result);
-            return true;
-          } else {
-            //
-            return Promise.reject("not a valid menu");
-          }
-        });
-      }),
-    ],
-    cart_controller.remove_item
-  );
-
-  // // Item Details
-  // app.get("/api/item/:id", isAuth, cart_controller.item_details);
-
-  // View Customer Cart Items
-  app.get("/api/cart_items/", isAuth, cart_controller.cart_items);
-
-  app.put("/api/empty_cart/", isAuth, cart_controller.empty_cart);
+  // get cart
+  app.get("/api/get_cart/", isAuth, cartController.getCart);
+  // Update cart
+  app.post("/api/update_cart/", isAuth, cartController.updateCart);
 };
