641af53be8995513c344d79db34038c633d2ab58
Windows10 <prem.shyam01@gmail.com>
2020-08-09 17:09:55 +0530

    Changes to cart schema

--- models/Cart.js
+++ models/Cart.js
@@ -4,20 +4,46 @@ const Schema = mongoose.Schema;
 mongoose.set("useFindAndModify", false);
 
 let cartSchema = new Schema({
-  customer_id: {
+  customer: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Customers",
     required: true,
   },
-  cart: [
+  caterer: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Caterer",
+      required: false,
+    },
+    name: { type: String, required: false },
+    image: { type: String, required: false },
+  },
+  cartItems: [
     {
-      menu_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Menu",
-        required: true,
-      },
-      cartItems: { type: {}, required: true },
-      quantity: { type: Number, required: true },
+      cuisineCategoryName: { type: String, required: true },
+      cuisineCategoryDescription: { type: String, required: true },
+      cuisineCategoryImage: { type: String, required: true },
+      cuisineCategoryId: { type: String, required: true },
+      minPlates: { type: Number, required: true },
+      totalPlates: { type: Number, required: true },
+      specialInstruction: { type: String, required: true },
+      price: { type: Number, required: true },
+      perUnit: { type: String, required: true },
+      menuDetails: [
+        {
+          id: { type: String, required: true },
+          menuItemCategory: {
+            id: { type: String, required: true },
+            category: { type: String, required: true },
+          },
+          menuItems: [
+            {
+              id: { type: String, required: true },
+              name: { type: String, required: true },
+            },
+          ],
+        },
+      ],
     },
   ],
 });
